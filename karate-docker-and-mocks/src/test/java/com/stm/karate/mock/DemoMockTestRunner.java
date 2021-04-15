4c7fc6a069bbfa102b400d8820ca547e5008a813
pedda <pquiel@wescale.com>
2021-04-15 08:16:53 +0200

    add karate mock server extension

--- karate-docker-and-mocks/src/test/java/com/stm/karate/mock/DemoMockTestRunner.java
+++ karate-docker-and-mocks/src/test/java/com/stm/karate/mock/DemoMockTestRunner.java
@@ -3,27 +3,31 @@ package com.stm.karate.mock;
 import com.intuit.karate.core.MockServer;
 import com.intuit.karate.junit5.Karate;
 import com.intuit.karate.resource.ResourceUtils;
+import org.junit.Rule;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.io.File;
 
 /**
- * This runner
+ * This is a JUnit runner for the `demo-mock-test.feature`.
+ *
+ * It starts a Karate Mock `demo-mock.feature` and runs the
+ * `demo-mock-test.feature`.
+ *
  */
-public class DemoMockTestRunner {
+public class DemoMockTestRunner  {
 
     private static MockServer mockServer;
 
+    @RegisterExtension
+    static KarateMockServerExtension mockServerRule = KarateMockServerExtension.create(DemoMockTestRunner.class, "demo-mock.feature");
+
     @BeforeAll
     static void startMockServer() {
-
-        final File mockFile = ResourceUtils.getFileRelativeTo(DemoMockTestRunner.class, "demo-mock.feature");
-        mockServer = MockServer.feature(mockFile).build();
-
-        // 'classpath:' prefix  is supported
-        // Relative resource resolution isn't
-        // mockServer = MockServer.feature("classpath:com/stm/karate/mock/").build();
+//        final File mockFile = ResourceUtils.getFileRelativeTo(DemoMockTestRunner.class, "demo-mock.feature");
+//        mockServer = MockServer.feature(mockFile).build();
     }
 
     @AfterAll
@@ -37,8 +41,8 @@ public class DemoMockTestRunner {
     @Karate.Test
     Karate testDemoMockServer() {
 
-        final Karate runner = Karate.run("test-demo-mock");
-        runner.builder().systemProperty("mock_server_port", String.valueOf(mockServer.getPort()));
+        final Karate runner = Karate.run("demo-mock-test");
+        runner.builder().systemProperty("mock_server_port", String.valueOf(mockServerRule.getPort()));
         return runner.relativeTo(getClass());
     }
 }
